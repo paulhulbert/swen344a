fc16206e7a08b6227022a061528fa6f860d3b999
Bill Tarr <betarr13@gmail.com>
2019-02-20 10:23:18 -0500

    basic react boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+import 'semantic-ui-css/semantic.min.css';
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <h1>
+        Hello World!
+      </h1>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ttf$/,
+        use: [
+          {
+            loader: 'ttf-loader',
+            options: {
+              name: './font/[hash].[ext]',
+            },
+          },
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|woff|woff2|eot|svg|)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    contentBase: './public',
+    hot: true
+  }
+};

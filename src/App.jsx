3ef47892de6b35d6a216b4813aeeff79772ee892
Bill Tarr <betarr13@gmail.com>
2019-02-23 11:47:35 -0500

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React, { PureComponent } from 'react'
 import HelloWorld from './components/HelloWorld';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 
 export default class App extends PureComponent {
   render() {
     return (
-      <HelloWorld />
+      <Router>
+        <Route exact path="/" component={HelloWorld} />
+      </Router>
     )
   }
 }

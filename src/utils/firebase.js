765e76be2232d556397be7b4010b2b7c2623cb4d
Bill Tarr <betarr13@gmail.com>
2019-03-02 11:05:09 -0500

    add config file, environment variable support

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,8 +1,12 @@
 import firebase from 'firebase/app';
 
-const app = firebase.initializeApp({
-    apiKey: "AIzaSyBwyxoKLx5ybSmVM5pB1fFBVAz6bX1qMk8",
-    authDomain: "web-engineering-spring-2019.firebaseapp.com"
-});
+const config = {
+  firebase: {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+  }
+};
+
+const app = firebase.initializeApp(config.firebase);
 
 export default app;

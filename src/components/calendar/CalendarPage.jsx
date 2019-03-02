8638095d518d633774adad8b3f4b391feeed6047
Mackenzie B <mmb8830@g.rit.edu>
2019-03-02 16:10:26 -0500

    Use React tags, put component on the grid, remove unused defaults

--- src/components/calendar/CalendarPage.jsx
+++ src/components/calendar/CalendarPage.jsx
@@ -1,34 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Grid, Container } from 'semantic-ui-react';
 import '../../../node_modules/fullcalendar-reactwrapper/dist/css/fullcalendar.min.css';
 import FullCalendar from 'fullcalendar-reactwrapper';
 
-class CalendarComponent extends React.Component {
-  render() {
-    return (
-      <div id="calendar-component">
-        <FullCalendar
-          id="main-calendar"
-          header={{
-            left: 'prev, next today myCustomButton',
-            center: 'title',
-            right: 'month,basicWeek,basicDay'
-          }}
-          defaultDate={'2019-03-03'}
-          navLinks={true}
-          editable={true}
-          eventLimit={true}
-        />
-      </div>
-    )
-  }
-}
-
-
 export default function CalendarPage() {
   return (
-    <div id="calendar-page">
-      <CalendarComponent/>
-    </div>
+    <Container>
+      <Grid
+        columns={1}
+        stretched={true}
+      >
+        <Grid.Column>
+          <FullCalendar
+            id="main-calendar"
+            header={{
+              left: 'prev, next today myCustomButton',
+              center: 'title',
+              right: 'month,basicWeek,basicDay'
+            }}
+            navLinks={true}
+            editable={true}
+            eventLimit={true}
+          />
+        </Grid.Column>
+      </Grid>
+    </Container>
   )
 }

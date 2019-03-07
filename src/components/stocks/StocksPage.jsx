a4b6190fe25de318f3865a5963268ac10a3aaf4d
Bill Tarr <betarr13@gmail.com>
2019-03-06 23:20:20 -0500

    fetch real stock data and chart it on stocks page

--- src/components/stocks/StocksPage.jsx
+++ src/components/stocks/StocksPage.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { StocksChart } from './StocksChart';
+import { Map } from 'immutable';
+import LoadingState from '../common/LoadingState';
+import { fetchOneYearStockChartData } from '../../utils/stocks/stocksUtils';
 
-export default function StocksPage() {
-  return (
-    <h1>
-      Stocks Page
-    </h1>
-  )
+export default class StocksPage extends PureComponent {
+
+  constructor() {
+    super();
+    this.state = {
+      chartDataByTicker: Map(),
+      selectedTicker: 'AAPL',
+      fetchingChartData: false,
+    }
+    this.handleAddStockDataForTicker = this.handleAddStockDataForTicker.bind(this);
+    this.handleSelectTicker = this.handleSelectTicker.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleSelectTicker('AAPL');
+  }
+
+  handleAddStockDataForTicker(ticker, stockData) {
+    this.setState({
+      chartDataByTicker: this.state.chartDataByTicker.set(ticker, stockData),
+    })
+  }
+
+  handleSelectTicker(selectedTicker) {
+    if (selectedTicker && !this.state.chartDataByTicker.get(selectedTicker) && !this.state.fetchingChartData) {
+      this.setState({
+        fetchingChartData: true,
+        selectedTicker,
+      });
+      fetchOneYearStockChartData(selectedTicker, this.handleAddStockDataForTicker)
+    } else {
+      this.setState({
+        fetchingChartData: false,
+        selectedTicker,
+      });
+    }
+  }
+
+  render() {
+    const stockChartData = this.state.chartDataByTicker.get(this.state.selectedTicker);
+    if (!stockChartData) {
+      return <LoadingState />
+    }
+    return (
+      <StocksChart stockTicker="AAPL" stockChartData={stockChartData}/>
+    );
+  }
 }
--- src/utils/stocks/stocksUtils.js
+++ src/utils/stocks/stocksUtils.js
@@ -1,5 +1,9 @@
 import { DEFAULTS_STOCKS, STOCK_FIELDS_TO_KEEP } from '../../constants/stocksConstants';
-import { fromJS } from 'immutable';
+import { fromJS, Map } from 'immutable';
+import { CHART_DAY_FIELDS, CHART_DATE_FORMATS, API_DATE_FORMAT } from '../../constants/stockChartConstants';
+import moment from 'moment';
+
+const BASE_URL = 'https://api.iextrading.com/1.0/stock';
 
 function processIndividualStockData(stock) {
     return stock.get('quote').filter((_, key) => {
@@ -20,9 +24,37 @@ export function fetchTopStocks(callback) {
 }
 
 function fetchStockData(stocks, callback){
-    fetch(`https://api.iextrading.com/1.0/stock/market/batch/?symbols=${getStockSymbols(stocks)}&types=quote`)
+    fetch(`${BASE_URL}/market/batch/?symbols=${getStockSymbols(stocks)}&types=quote`)
         .then((resp) => resp.json())
         .then(fromJS)
         .then(processRawStocks)
         .then(callback);
+}
+
+// chart utils
+
+export function getChartLabels(chartType, chartData) {
+    return chartData.map((dataPoint) => {
+        const pointDate = moment(dataPoint.get(CHART_DAY_FIELDS.DATE), API_DATE_FORMAT);
+        return pointDate.format(CHART_DATE_FORMATS[chartType]);
+    })
+}
+
+function parseStockChartData(rawData) {
+    return rawData.map((dayData) => {
+        return Map({
+            [CHART_DAY_FIELDS.CLOSE]: dayData.get(CHART_DAY_FIELDS.CLOSE),
+            [CHART_DAY_FIELDS.DATE]: dayData.get(CHART_DAY_FIELDS.DATE),
+        })
+    });
+}
+
+export function fetchOneYearStockChartData(ticker, callback) {
+    fetch(`${BASE_URL}/${ticker}/chart/1y`)
+        .then((resp) => resp.json())
+        .then(fromJS)
+        .then(parseStockChartData)
+        .then((chartData) => {
+            callback(ticker, chartData)
+        });
 }

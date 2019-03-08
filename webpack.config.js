11a68a497107ccd2a8a8c3748e9b3ee2e6d872ce
Bill Tarr <betarr13@gmail.com>
2019-03-08 09:48:30 -0500

    add eslint with babel preset

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,11 @@ module.exports = {
         use: ['babel-loader']
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },

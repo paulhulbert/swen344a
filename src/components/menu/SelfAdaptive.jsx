088aa05eec853a37c46b026852905e4859f52fc7
Paul Hulbert <pxh8242@rit.edu>
2019-04-22 13:27:22 -0400

    Stores history in database

--- src/components/menu/SelfAdaptive.jsx
+++ src/components/menu/SelfAdaptive.jsx
@@ -1,5 +1,10 @@
 import React, { PureComponent } from 'react';
 import {fromJS} from "immutable";
+import {CACHE_LENGTH_MINS} from "../../constants/cacheConstants";
+import { DATE_FORMAT } from '../../constants/formats';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import moment from 'moment';
 
 export default class SelfAdaptive extends PureComponent {
 
@@ -35,11 +40,20 @@ export default class SelfAdaptive extends PureComponent {
                 .then(response => response.json())
                 .then(fromJS)
                 .then((response) => {
+                    this.writeSelfAdaptiveData(total, response.get('load'));
                     this.fetchServersUp(++serverNumber, (total * 10 - response.get('load') > 0))
                 });
         } else {
             this.handleServerUpdate(total)
         }
+    }
+
+    writeSelfAdaptiveData(serverCount, load) {
+        const fetchMoment = moment().format();
+        firebase.database().ref("SelfAdaptive/" + fetchMoment).set({
+            serverCount: serverCount,
+            load: load,
+        });
     }
 
     render() {
